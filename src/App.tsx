7ddcca768f4573a6f349614e0035dabc2b277880
aries0d0f <aries0d0f@gmail.com>
2023-06-03 13:23:53 +0800

    feat: Add GPU rendering to canvas.
    This change adds WebGPU rendering to the canvas element. Underlying GPU compute context, pipeline, vertex buffers, command encoder were created, adjusted, and used to render triangle to the screen. The function that generates all this set was added as an effect to be called on component mount and on each change of the GPU and ctx states.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,135 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createRef, useEffect, useState } from 'react';
+
+import shaderCode from './shader.wgsl?raw';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef$ = createRef<HTMLCanvasElement>();
+  const [GPU, setGPU] = useState<GPUDevice>();
+  const [ctx, setCtx] = useState<GPUCanvasContext>();
+
+  const GPUComputeCtx = (GPU: GPUDevice, ctx: GPUCanvasContext) => {
+    ctx.configure({
+      device: GPU,
+      format: navigator.gpu.getPreferredCanvasFormat(),
+      alphaMode: 'premultiplied'
+    });
+
+    const shaderModule = GPU.createShaderModule({
+      code: shaderCode
+    });
+
+    const vertices = new Float32Array([
+      0,     0.6, 0, 1, // position  [x, y, z, w]
+      1,       0, 0, 1, // color     [r, g, b, a]
+      -0.5, -0.6, 0, 1, // position  [x, y, z, w]
+      0,       1, 0, 1, // color     [r, g, b, a]
+      0.5,  -0.6, 0, 1, // position  [x, y, z, w]
+      0,       0, 1, 1  // color     [r, g, b, a]
+    ]);
+
+    const vertexBuffer = GPU.createBuffer({
+      size: vertices.byteLength, // make it big enough to store vertices in
+      usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST
+    });
+
+    GPU.queue.writeBuffer(vertexBuffer, 0, vertices, 0, vertices.length);
+
+    const vertexBuffers = [
+      {
+        attributes: [
+          {
+            shaderLocation: 0, // position
+            offset: 0,
+            format: 'float32x4'
+          },
+          {
+            shaderLocation: 1, // color
+            offset: 16,
+            format: 'float32x4'
+          }
+        ],
+        arrayStride: 32,
+        stepMode: 'vertex'
+      }
+    ] satisfies GPUVertexBufferLayout[];
+
+    const pipelineDescriptor = {
+      vertex: {
+        module: shaderModule,
+        entryPoint: 'vertex_main',
+        buffers: vertexBuffers
+      },
+      fragment: {
+        module: shaderModule,
+        entryPoint: 'fragment_main',
+        targets: [
+          {
+            format: navigator.gpu.getPreferredCanvasFormat()
+          }
+        ]
+      },
+      primitive: {
+        topology: 'triangle-list'
+      },
+      layout: 'auto'
+    } satisfies GPURenderPipelineDescriptor;
+
+    const renderPipeline = GPU.createRenderPipeline(pipelineDescriptor);
+
+    const commandEncoder = GPU.createCommandEncoder();
+
+    const clearColor = { r: 0.0, g: 0.5, b: 1.0, a: 1.0 };
+
+    const renderPassDescriptor = {
+      colorAttachments: [
+        {
+          clearValue: clearColor,
+          loadOp: 'clear',
+          storeOp: 'store',
+          view: ctx.getCurrentTexture().createView()
+        }
+      ]
+    } satisfies GPURenderPassDescriptor;
+
+    const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+
+    passEncoder.setPipeline(renderPipeline);
+    passEncoder.setVertexBuffer(0, vertexBuffer);
+    passEncoder.draw(3);
+    passEncoder.end();
+
+    GPU.queue.submit([commandEncoder.finish()]);
+  };
+
+  useEffect(() => {
+    navigator.gpu
+      .requestAdapter()
+      .then((adapter) => adapter?.requestDevice())
+      .then((device) => setGPU(device));
+  }, []);
+
+  useEffect(() => {
+    if (canvasRef$.current && canvasRef$.current.getContext('webgpu')) {
+      canvasRef$.current.width = canvasRef$.current.clientWidth;
+      canvasRef$.current.height = canvasRef$.current.clientHeight;
+
+      setCtx(canvasRef$.current.getContext('webgpu')!);
+    }
+  }, [canvasRef$]);
+
+  useEffect(() => {
+    if (GPU && ctx) {
+      GPUComputeCtx(GPU, ctx);
+    }
+  }, [GPU, ctx]);
 
   return (
     <>
-      <canvas />
+      <canvas ref={canvasRef$} />
     </>
   );
 }
 
-export default App
+export default App;
